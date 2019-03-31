161364dd7e7bedbfa79e65d653bc47fd12b65514
Muhammad Arslan <gery_maz_555@hotmail.com>
2019-03-31 08:43:32 +0500

    add more files

--- 02Basics/marvels.js
+++ 02Basics/marvels.js
@@ -2,9 +2,11 @@
 
 const superHeroes = ['Iron Man','Spider Man','Capt. America','Thor']
 
+
 console.log(superHeroes);
 console.log(superHeroes[0]);
 console.log(superHeroes[2]);
+
 
 console.log(superHeroes[superHeroes.length-1]);
 
---
+++ 02Basics/moreArray.js
@@ -0,0 +1,8 @@
+
+const values = ['one','two','three','four','five','six','seven','eight','nine','ten']
+const numbers = [1,2,3,4,5,6,7,8,9,10]
+
+console.log(values[values.length-1]);
+console.log(`we have ${values.length}, values`)
+
+console.log( numbers[0]+numbers[3])
