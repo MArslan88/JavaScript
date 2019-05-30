985b1be93950e11ac9bc87196a6dff2450a7ed97
Muhammad Arslan <gery_maz_555@hotmail.com>
2019-05-30 17:25:48 +0500

    Concept about Objects

---
+++ 02Intermediate/myObjects.js
@@ -0,0 +1,12 @@
+let myYoutubeVideo = {
+    title: 'Loops in JavaScript',
+    videoLength: '15',
+    videoCreator: 'Muhammad Arslan',
+    videoDescription: 'this is a video description and a long one'
+}
+
+//console.log(myYoutubeVideo);
+
+
+//this will fetch the specific key values from the object like 'myYoutubeVideo.title' this will fetch only the title
+console.log(`Hey new Video on ${myYoutubeVideo.title} by ${myYoutubeVideo.videoCreator}`);
