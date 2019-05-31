72b5aa485833d08eb61a84a8676dba8194498625
Muhammad Arslan <gery_maz_555@hotmail.com>
2019-05-31 05:31:35 +0500

    Assignment

---
+++ 02Intermediate/assignment.js
@@ -0,0 +1,8 @@
+let course = {
+    name: 'JavaScript',
+    author: 'Muhammad Arslan',
+    price: 500,
+    description: `This Course is about JavaScript`
+}
+
+console.log(`${course.description} and written by ${course.author} and the price of this course is Rs ${course.price}`);
--- 02Intermediate/myObjects.js
+++ 02Intermediate/myObjects.js
@@ -1,6 +1,13 @@
-let myYoutubeVideo = {
+let myYoutubeVideoOne = {
     title: 'Loops in JavaScript',
-    videoLength: '15',
+    videoLength: 15,
+    videoCreator: 'Muhammad Arslan',
+    videoDescription: 'this is a video description and a long one'
+}
+
+let myYoutubeVideoTwo = {
+    title: 'Functions in JavaScript',
+    videoLength: 10,
     videoCreator: 'Muhammad Arslan',
     videoDescription: 'this is a video description and a long one'
 }
@@ -9,4 +16,13 @@ let myYoutubeVideo = {
 
 
 //this will fetch the specific key values from the object like 'myYoutubeVideo.title' this will fetch only the title
-console.log(`Hey new Video on ${myYoutubeVideo.title} by ${myYoutubeVideo.videoCreator}`);
+//console.log(`Hey new Video on ${myYoutubeVideo.title} by ${myYoutubeVideo.videoCreator}`);
+
+let changeVideoLength = function(myObject){
+    return {
+        formateOne: `Time of this video is: ${myObject.videoLength + 2}`,
+        formateTwo: `Time of this video is: ${myObject.videoLength + 1}`
+    }
+}
+let adOne =changeVideoLength(myYoutubeVideoTwo);
+console.log(adOne.formateTwo);
