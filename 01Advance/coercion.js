e55e640968dad295b28f811a947a29a36f368d99
Muhammad Arslan <gery_maz_555@hotmail.com>
2019-06-14 08:51:18 +0500

    Coercion

---
+++ 01Advance/coercion.js
@@ -0,0 +1,27 @@
+// console.log('5'+ 5);
+
+// const giveType = typeof 5
+// console.log(giveType);
+
+const adder = 5 + false
+
+// console.log(adder);
+
+const loginDetails = []
+// logic for getting details from DB
+
+const loginID = loginDetails[0]
+
+if(loginDetails){
+    console.log('Allow user to login')    
+}else{
+    console.log('Auth failed');    
+}
+
+// values that interpret as false:
+// false
+// 0
+// ''
+// null
+// undefined
+
